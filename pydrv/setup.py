98742c7c5bafe2628586dba365032539522e1ad5
Popax21 <popa21@popa21.at>
2021-09-02 20:49:14 +0200

    Fixed package installation script

--- pydrv/setup.py
+++ pydrv/setup.py
@@ -7,4 +7,4 @@ def list_files(d):
         for f in files: fs.append(os.path.join(root, f))
     return fs
 
-setup(name="tudor", version="1.0.0", packages=["tudor"], package_data={'': list_files("tudor")}, python_requires='>3',install_requires=["cryptography", "pyusb", "matplotlib"])
+setup(name="tudor", version="1.0.0", packages=["tudor"], package_data={'': list_files("%s/tudor" % os.path.dirname(os.path.realpath(__file__))) }, python_requires='>3', install_requires=["cryptography", "pyusb", "matplotlib"])
