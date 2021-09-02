abde573b126ed621154d8194b66efbbe05be78a7
Popax21 <popa21@popa21.at>
2021-09-02 20:42:00 +0200

    Fixed setup script

--- pydrv/setup.py
+++ pydrv/setup.py
@@ -1,3 +1,10 @@
+import os
 from distutils.core import setup
 
-setup(name="tudor", version="1.0.0", packages=["tudor"], python_requires='>3',install_requires=["cryptography", "pyusb", "matplotlib"])
+def list_files(d):
+    fs = []
+    for (root, dirs, files) in os.walk(d):
+        for f in files: fs.append(os.path.join(root, f))
+    return fs
+
+setup(name="tudor", version="1.0.0", packages=["tudor"], package_data={'': list_files("tudor")}, python_requires='>3',install_requires=["cryptography", "pyusb", "matplotlib"])
