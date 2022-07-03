722d31266de03bb0f060db12457106957fa198aa
Popax21 <popa21@popa21.at>
2022-07-03 17:44:24 +0200

    Finalize and export libfprint IPC protocol header

---
+++ libfprint-host/inc/tudor/libfprint-proto.h
@@ -0,0 +1,99 @@
+#ifndef TUDOR_TUDOR_LIBFPRINT_PROTO_H
+#define TUDOR_TUDOR_LIBFPRINT_PROTO_H
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+#include <tudor/log.h>
+#include <tudor/tudor.h>
+
+enum ipc_msg_type {
+    IPC_MSG_INIT,
+    IPC_MSG_SANDBOX,
+    IPC_MSG_READY,
+    IPC_MSG_SHUTDOWN,
+    IPC_MSG_ACK,
+
+    IPC_MSG_ADD_RECORD,
+    IPC_MSG_DEL_RECORD,
+
+    IPC_MSG_ENROLL,
+    IPC_MSG_RESP_ENROLL,
+    IPC_MSG_VERIFY,
+    IPC_MSG_RESP_VERIFY,
+    IPC_MSG_IDENTIFY,
+    IPC_MSG_RESP_IDENTIFY
+};
+
+#define IPC_SENSOR_NAME_SIZE 32
+#define IPC_MAX_PAIR_DATA_SIZE 1024
+#define IPC_MAX_RECORD_SIZE 4096
+
+struct ipc_msg_init {
+    enum ipc_msg_type type;
+
+    enum log_level log_level;
+    uint8_t usb_bus, usb_addr;
+
+    char sensor_name[IPC_SENSOR_NAME_SIZE];
+    uint8_t pair_data[];
+};
+
+struct ipc_msg_ready {
+    enum ipc_msg_type type;
+
+    char sensor_name[IPC_SENSOR_NAME_SIZE];
+    uint8_t pair_data[];
+};
+
+struct ipc_msg_add_record {
+    enum ipc_msg_type type;
+
+    RECGUID guid;
+    enum tudor_finger finger;
+    uint8_t record_data[];
+};
+
+struct ipc_msg_del_record {
+    enum ipc_msg_type type;
+
+    RECGUID guid;
+    enum tudor_finger finger;
+};
+
+struct ipc_msg_enroll {
+    enum ipc_msg_type type;
+
+    RECGUID guid;
+    enum tudor_finger finger;
+};
+
+struct ipc_msg_resp_enroll {
+    enum ipc_msg_type type;
+
+    uint8_t record_data[];
+};
+
+struct ipc_msg_verify {
+    enum ipc_msg_type type;
+
+    RECGUID guid;
+    enum tudor_finger finger;
+};
+
+struct ipc_msg_resp_verify {
+    enum ipc_msg_type type;
+
+    bool did_match;
+};
+
+struct ipc_msg_resp_identify {
+    enum ipc_msg_type type;
+
+    bool did_match;
+    RECGUID guid;
+    enum tudor_finger finger;
+};
+
+#endif
--- libfprint-host/src/ipc.h
+++ libfprint-host/src/ipc.h
@@ -1,7 +1,7 @@
 #ifndef LIBFPRINT_HOST_IPC_H
 #define LIBFPRINT_HOST_IPC_H
 
-#include "proto.h"
+#include <tudor/libfprint-proto.h>
 
 size_t ipc_recv_msg(int sock, void *buf, enum ipc_msg_type type, size_t min_sz, size_t max_sz);
 void ipc_send_msg(int sock, void *buf, size_t size);
